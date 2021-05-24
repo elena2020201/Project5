b010b6714828d6669b6c3124f6e3975b0cdb7b51
elena2020201 <elenarus1992@mail.ru>
2021-05-24 19:04:49 +0300

    Добавьте файлы проекта.

---
+++ сумма, пока не введен 0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static int Main()
+    {
+        /*переменная, которой потом присвоим значение этой суммы, 
+        должна сразу быть инициализирована нулем*/
+        double res = 0;
+        for (; ; )
+        {
+            Console.Write("Введите число, для вывода суммы этих чисел введите 0 : ");
+            double num = double.Parse(Console.ReadLine());
+            if (num == 0)
+            {
+                break;
+            }
+            res += num;
+        }
+        Console.WriteLine("\nСумма : {0}", res);
+        Console.ReadKey();
+        return 0;
+    }
+}
